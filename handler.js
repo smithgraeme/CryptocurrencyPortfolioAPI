6d3c2e4d66ccda70dd4b546cdfd27e702d946b81
Graeme Smith <30961431+smithgraeme@users.noreply.github.com>
2018-12-01 15:54:08 -0700

    update template

--- handler.js
+++ handler.js
@@ -9,6 +9,4 @@ module.exports.hello = async (event, context) => {
     }),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
