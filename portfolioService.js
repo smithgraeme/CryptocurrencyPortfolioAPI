d8ce1b4dcba313c10cc0eb4ffdb92cbc59a03ae6
Graeme Smith <30961431+smithgraeme@users.noreply.github.com>
2018-12-01 21:43:27 -0700

    Working portfolio microservice

--- portfolioService.js
+++ portfolioService.js
@@ -1,8 +1,11 @@
 const AWS = require("aws-sdk");
-const dynamo = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const uuidv4 = require('uuid/v4');
+const crypto = require('crypto');
+
+const tableName = process.env.table;
 
 module.exports.createPortfolio = async (event, context) => {
-
   //console.log(JSON.stringify(event,null,2));
 
   //console.log(event.body);
@@ -12,11 +15,15 @@ module.exports.createPortfolio = async (event, context) => {
   console.log(JSON.stringify(parsedBody,null,2));
   //console.log(parsedBody);
 
+  const uuid = uuidv4().replace(/-/g, ''); //the dashes are annoying in URLs
+  const hashedUuid = getHashedValue(uuid);
 
-  var params = {
+  console.log(uuid);
+
+  const params = {
     Item: {
      "identifierHash": {
-       S: parsedBody.name
+       S: hashedUuid
       },
       "name": {
         S: parsedBody.name
@@ -25,13 +32,61 @@ module.exports.createPortfolio = async (event, context) => {
        S: JSON.stringify(parsedBody.coins)
       }
     },
-    TableName: process.env.table
+    TableName: tableName
   };
 
-  await dynamo.putItem(params).promise();
+  await dynamodb.putItem(params).promise();
 
   return {
     statusCode: 200,
+    body: JSON.stringify({
+      portfolioIdentifier: uuid,
+      //hashedUuid: hashedUuid
+    })
   };
-
 };
+
+module.exports.getPortfolio = async (event, context) => {
+  console.log(JSON.stringify(event,null,2));
+
+  var portfolioId;
+
+  //support loading the portfolio ID as both a query parameter or a path parameter
+  //if one fails, just try the other
+  try{
+    portfolioId = event.queryStringParameters.portfolioId
+  } catch(error) {
+    portfolioId = event.pathParameters.id;
+  }
+
+  console.log(portfolioId,null,2);
+
+  const portfolioIdHashed = getHashedValue(portfolioId);
+
+  const params = {
+    Key: {
+     "identifierHash": {
+       S: portfolioIdHashed
+      },
+    },
+    TableName: tableName
+   };
+
+  const result = await dynamodb.getItem(params).promise();
+  console.log(result);
+
+  const coins = JSON.parse(result.Item.coins.S);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      coins: coins
+    })
+  };
+}
+
+function getHashedValue(value){
+  const hash = crypto.createHash('sha256');
+  hash.update(value);
+  return hash.digest('hex');
+}
